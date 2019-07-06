a47ac22c33608db4d58075292b28a9057e5dfee1
Yogesh Sreedharan <yogesh.s.aux@go-jek.com>
2019-07-06 19:18:22 +0530

    kafka-protobuf-console-consumer-1.0.tar.gz

---
+++ kafka-protobuf-console-consumer.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class KafkaProtobufConsoleConsumer < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/yogeshsr/kafka-protobuf-console-consumer/raw/master/kafka-protobuf-console-consumer-1.0.tar.gz"
+  sha256 "3b1af09a67c2766cb0c8664d69c096a8905ba3e7c5f683676cb36f57a9e95e64"
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    system "make", "install" # if this fails, try separate make/make install steps
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test kafka-protobuf-console-consumer`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
