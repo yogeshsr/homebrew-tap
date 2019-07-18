15362379906af076a1f8b22b7b3c7db1978357a2
Yogesh Sreedharan <yogesh.s.aux@go-jek.com>
2019-07-18 22:21:24 +0530

    adding version info

--- kafka-protobuf-console-consumer.rb
+++ kafka-protobuf-console-consumer.rb
@@ -6,6 +6,7 @@ class KafkaProtobufConsoleConsumer < Formula
   homepage ""
   url "https://github.com/yogeshsr/kafka-protobuf-console-consumer/raw/master/kafka-protobuf-console-consumer-2.0.0.tar.gz"
   sha256 "e17c4bee88a52ae11e003d30f42340fd7853af86abc39d5089a10f30050dc2f0"
+  version "2.0.0"
   # depends_on "cmake" => :build
 
   def install
