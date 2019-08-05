75bcc63a3413777b02c257d2861b655b443b3717
Yogesh Sreedharan <yogesh.s.aux@go-jek.com>
2019-08-05 22:07:11 +0530

    version bumped

--- kafka-protobuf-console-consumer.rb
+++ kafka-protobuf-console-consumer.rb
@@ -1,9 +1,9 @@
 class KafkaProtobufConsoleConsumer < Formula
   desc ""
   homepage ""
-  url "https://github.com/yogeshsr/kafka-protobuf-console-consumer/raw/master/kafka-protobuf-console-consumer-2.0.1.tar.gz"
-  sha256 "a79a0b141d70a2a7bbf5c987b41a6054b42875dcfe19602946e5572561689aa3"
-  version "2.0.1"
+  url "https://github.com/yogeshsr/kafka-protobuf-console-consumer/raw/master/kafka-protobuf-console-consumer-2.1.0.tar.gz"
+  sha256 "9cfafaacceed1bb6b8b8eac3157765d2c768b7b7dbc902f49caae8222b450ef1"
+  version "2.1.0"
   # depends_on "cmake" => :build
 
   def install
@@ -11,6 +11,6 @@ class KafkaProtobufConsoleConsumer < Formula
   end
 
   test do
-    assert_equal %x{kafka-protobuf-console-consumer -v}, "Version: 2.0.1\n"
+    assert_equal %x{kafka-protobuf-console-consumer -v}, "Version: 2.1.0\n"
   end
 end
